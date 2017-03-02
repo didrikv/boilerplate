99575d7d663d44cfa1562b09634b47bc6e24eacb
Didrik Vareide <dvareide@gmail.com>
2017-03-02 18:34:05 +0100

    I dont giva a fuck!

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,13 @@
 import React from "react"
 import ReactDom from "react-dom"
 import { Provider } from "react-redux"
+import Chart from "./components/Chart.js"
 
 import store from "./store.js"
 
 const app = document.getElementById("app")
 
 ReactDom.render(
-	<p>Jon Gudmund, slutt aa drikk saa mye!!</p>
+	<Chart/>
 ,app)
 
