226efa3e412bcdb885fe1706d9ee172795100ba4
Didrik Vareide <dvareide@gmail.com>
2017-08-30 13:30:27 +0200

    I dont giva a fuck!

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ const express = require("express")
 var expressStaticGzip = require("express-static-gzip")
 const app = express()
 
-app.use(expressStaticGzip(__dirname + "/src/build"))
+app.use(expressStaticGzip(__dirname + "/public"))
+app.use(expressStaticGzip(__dirname))
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + "/src/build/index.html")
+	res.sendFile(__dirname + "/index.html")
 })
 
-app.get('/test', (req, res) => {
-	res.sendFile(__dirname + "/src/build/index.html")
+app.get('/attraktivitet/*', (req, res) => {
+	res.sendFile(__dirname + "/public/attraktivitet/index.html")
+})
+
+app.get('/kultur/*', (req, res) => {
+	res.sendFile(__dirname + "/public/kultur/index.html")
 })
 
 app.listen(3000, () => {
 	console.log("Server running, and listening on port 3000")
 })
-
