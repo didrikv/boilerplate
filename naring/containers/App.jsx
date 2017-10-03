232711d7aaba9a04766f11be79fc540dd30bc74c
Didrik Vareide <dvareide@gmail.com>
2017-10-03 12:28:57 +0200

    I dont giva a fuck!

--- naring/containers/App.jsx
+++ naring/containers/App.jsx
@@ -5,23 +5,36 @@ import Footer from './Footer.jsx'
 import KategoriRoute from './KategoriRoute.jsx'
 import Steder from './Steder.jsx'
 
-export default function App(props) {
-	const kategori = (routeProps) => <KategoriRoute {...props} {...routeProps} />
-	const kommuner = (routeProps) => <Steder {...props} {...routeProps} inndeling='Kommune'/>
-	const regioner = (routeProps) => <Steder {...props} {...routeProps} inndeling='Region'/>
-	const fylker = (routeProps) => <Steder {...props} {...routeProps} inndeling='Fylke'/>
-	return(
-		<div>
-			<Router basename="/naring">
-				<div>
-					<Header/>
-					<Route path="/kategori" render={kategori} />
-					<Route path="/kommuner" render={kommuner} />
-					<Route path="/fylker" render={fylker} />
-					<Route path="/regioner" render={regioner} />
-					<Footer />
-				</div>
-			</Router>
-		</div>
-	)
+export default class App extends React.Component {
+	constructor(props){
+		super()
+
+		this.state = {
+			year: [2013, 2014, 2015],
+			variable: 'Produktivitet',
+			inndeling: 'Kommune'
+		}
+	}
+
+	kategori = (routeProps) => <KategoriRoute {...this.props} {...routeProps} Gstate={this.state} setGstate={ () => this.setState()} />
+	kommuner = (routeProps) => <Steder {...this.props} {...routeProps} inndeling='Kommune'/>
+	regioner = (routeProps) => <Steder {...this.props} {...routeProps} inndeling='Region'/>
+	fylker = (routeProps) => <Steder {...this.props} {...routeProps} inndeling='Fylke'/>
+
+	render() {
+		return(
+			<div>
+				<Router basename="/naring">
+					<div>
+						<Header/>
+						<Route path="/kategori" render={this.kategori} />
+						<Route path="/kommuner" render={this.kommuner} />
+						<Route path="/fylker" render={this.fylker} />
+						<Route path="/regioner" render={this.regioner} />
+						<Footer />
+					</div>
+				</Router>
+			</div>
+		)
+	}
 }
