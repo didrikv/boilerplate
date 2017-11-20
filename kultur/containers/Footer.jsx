2fba9042aa4829f8410aa7a49634085d4190885d
dvareide <dvareide@gmail.com>
2017-11-20 22:14:57 +0700

    I dont giva a fuck!

--- kultur/containers/Footer.jsx
+++ kultur/containers/Footer.jsx
@@ -11,17 +11,21 @@ export default function Footer(props) {
 		<div className={styles.footer}>
 			<Grid>
 				<Row>
-					<Col sm={4} style={{textAlign: 'center'}}>
+					<Col sm={3} style={{textAlign: 'center'}}>
 						<p> <a href="www.telemarksforsking.no/medarbeidere/detalj.asp?id=80&merket=6">Bård Kleppe</a> </p>
 						<p> <a href="mailto:kleppe@tmforsk.no">kleppe@tmforsk.no</a> </p>
 						<p>+47 911 97 543</p>
 					</Col>
-					<Col sm={4} style={{textAlign: 'center'}}>
+					<Col sm={3} style={{textAlign: 'center'}}>
 						<p> <a href="www.telemarksforsking.no">Telemarksforsking</a> </p>
-						<p>Postboks 4, 3833 Bø i Telemark</p>
+						<p style={{fontSize: '18px'}}>Postboks 4, 3833 Bø i Telemark</p>
 						<p>	+47 35 06 15 00</p>
 					</Col>
-					<Col sm={4} style={{textAlign: 'center'}}>
+					<Col sm={3} style={{textAlign: 'center'}}>
+						<p style={{fontSize: '18px'}}>Nettsiden er produsert av Didrik Vareide</p>
+						<p><a href="mailto:dvareide@gmail.com">dvareide@gmail.com</a></p>
+					</Col>
+					<Col sm={3} style={{textAlign: 'center'}}>
 						<p> © 2017 Telemarksforsking </p>
 						<p style={{fontSize: '18px'}}>Icons made by <a style={{fontSize: '18px'}} href="http://www.freepik.com" title="Freepik">Freepik</a> from <a style={{fontSize: '18px'}} href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a style={{fontSize: '18px'}} href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></p>
 					</Col>
